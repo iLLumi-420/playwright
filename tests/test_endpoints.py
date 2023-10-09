439e8b8bce972902b4edd525d246467121cd57f6
iLLumi-420 <sandeepparajuli96@gmail.com>
2023-10-09 15:35:14 +0545

    learn new things

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -11,17 +11,32 @@ client = TestClient(app)
 def test_get_started_link(page: Page):
     page.goto("https://playwright.dev/")
 
-    # Click the get started link.
+    python_link = page.locator('a[href="https://playwright.dev/python/docs/intro"]')
+    python_link.scroll_into_view_if_needed()
+
+    page.screenshot(path="scoll_to_link.jpg")
+
+    page.click('a[href="https://playwright.dev/python/docs/intro"]')
+    page.wait_for_load_state("load")
+
+    page.screenshot(path='new_page.jpg')
+
+    page.go_back()
+    page.wait_for_load_state("load")
+
+
     page.get_by_role("link", name="Get started").click()
 
-    # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
 
 def test_endpoints(page:Page):
     response = client.get('/')
     assert response.status_code == 200
 
     page.goto("http://localhost:8000/")
+
+    page.screenshot(path="screenshot.jpg")
         
     content = page.content()
 
